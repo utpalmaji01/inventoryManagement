c32a94eb38d66275c1120e5eb818700d30467848
Utpal Maji <utpalmaji.um@gmail.com>
2021-03-02 16:31:53 +0530

    add: some change

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const Header = () => {
+  return <div className="header-container"></div>;
+};
+
+export default Header;
--- src/pages/dashBoard/DashBoard.jsx
+++ src/pages/dashBoard/DashBoard.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React from "react";
+import Header from "../../components/Header/Header.jsx";
+import "./dashBoard.scss";
 const DashBoard = () => {
-    return ( <div className="dashboard-container"></div> );
-}
- 
+  return (
+    <div className="dashboard-container">
+      <div className="dashboard-header"></div>
+      <div className="dashboard-body">
+        <div className="dashboard-sideNav"></div>
+        <div className="dashboard-details"></div>
+      </div>
+    </div>
+  );
+};
+
 export default DashBoard;
